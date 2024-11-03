ded853834628d3ae59bc824a2fd5454e97fc57a4
Yurn <ChenDSakura@163.com>
2024-11-03 19:51:45 +0800

    fix

--- GameTime.cs
+++ GameTime.cs
@@ -16,6 +16,7 @@ public static class GameTime {
         var hours = time / 100 % 24;
         var hoursTimescale = hours * 6;
         var days = SDate.Now().DaysSinceStart;
+        if (hours < 6) days++;
         var daysTimescale = days * 24 * 6;
         return (timescale + hoursTimescale + daysTimescale) * TICKS_PER_TIMESCALE;
     }
