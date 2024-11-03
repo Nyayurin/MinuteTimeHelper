0e61703c1fb0190bef019884e16df3d97f7ee517
Yurn <ChenDSakura@163.com>
2024-11-03 18:51:27 +0800

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,16 @@
+﻿using StardewModdingAPI;
+
+namespace TickTimeHelper;
+
+internal class Mod : StardewModdingAPI.Mod {
+    private readonly TickTimeApi tickTimeApi = new();
+    public override void Entry(IModHelper helper) {
+        helper.Events.GameLoop.SaveLoaded += (_, _) => tickTimeApi.load();
+        helper.Events.GameLoop.UpdateTicking += (_, _) => tickTimeApi.update();
+        helper.Events.GameLoop.TimeChanged += (_, _) => tickTimeApi.sync();
+    }
+
+    public override TickTimeApi GetApi() {
+        return tickTimeApi;
+    }
+}
---
+++ TickTimeApi.cs
@@ -0,0 +1,29 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+
+namespace TickTimeHelper;
+
+public class TickTimeApi {
+    public long time { get; private set; }
+    public List<Action<long>> onLoad { get; } = new();
+    public List<Action<long>> onUpdate { get; } = new();
+    public List<Action<long, long>> onSync { get; } = new();
+    
+    internal void load() {
+        time = GameTime.parse(Game1.timeOfDay);
+        onLoad.ForEach(action => action(time));
+    }
+
+    internal void update() {
+        if (!Context.IsWorldReady || !Game1.shouldTimePass()) return;
+        time++;
+        onUpdate.ForEach(action => action(time));
+    }
+
+    internal void sync() {
+        var syncTicks = GameTime.parse(Game1.timeOfDay);
+        var delta = syncTicks - time;
+        time = syncTicks;
+        onSync.ForEach(action => action(time, delta));
+    }
+}
